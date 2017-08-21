cdf727adfbcf0d8fac1855b6e4948d338b232098
qwertyzzz0070 <0910gyu@naver.com>
2017-08-21 18:37:56 +0900

    MemberDAO -> MemberDao    changeClassName
    error part 주석처

---
+++ src/com/team/dao/MemberDao.java
@@ -0,0 +1,191 @@
+package com.team.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.team.dto.MemberDTO;
+import com.team.util.DBUtil;
+public class MemberDao {
+
+	// 회원정보 출력
+	public List<MemberDTO> memberList() {
+		Connection con = DBUtil.getConnection();
+		String sql = "select * from userInfo where id = ?";
+		List<MemberDTO> list = new ArrayList<>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			pstmt = con.prepareStatement(sql);
+			
+//			pstmt.setString(1, Member.userID);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				list.add(new MemberDTO(rs.getString("id"), 
+						rs.getString("name"),
+						rs.getString("mail"),
+						rs.getString("addr"),
+						rs.getString("phone")));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			DBUtil.close(con, pstmt, rs);
+		}
+		return list;
+	} // end of memberList
+	
+	// 이름 변경
+	public int updateName(String id) {
+		Connection con = DBUtil.getConnection();
+		PreparedStatement pstmt = null;
+		int result = 0;
+		String sql = "update table name set colume = ? "
+				+ " where colume = ?";
+		try {
+			
+			pstmt = con.prepareStatement(sql);
+			
+			// 바꿀 이름
+			pstmt.setString(1, id); 
+			
+			// 기존 이름
+//			pstmt.setString(2, MyPage.userId); 
+			
+			result = pstmt.executeUpdate();
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			DBUtil.close(con, pstmt);
+		}
+		return result;
+	}
+	
+	// 이메일 변경
+	public int updateMail(){
+		Connection con = DBUtil.getConnection();
+		PreparedStatement pstmt = null;
+		int result = 0;
+		String sql = "update table mail set colume = ? "
+				+ " where colume = ?";
+		try {
+			
+			pstmt = con.prepareStatement(sql);
+			
+//			// 바꿀 이메일
+//			pstmt.setString(1, id); 
+//			
+//			// 기존 이메일
+//			pstmt.setString(2, MyPage.userId); 
+			
+			result = pstmt.executeUpdate();
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			DBUtil.close(con, pstmt);
+		}
+		return result;
+		
+	}
+	
+	// 주소 변경
+	public int updateAddr(){
+		Connection con = DBUtil.getConnection();
+		PreparedStatement pstmt = null;
+		int result = 0;
+		String sql = "update table addr set colume = ? "
+				+ " where colume = ?";
+		try {
+//			
+//			pstmt = con.prepareStatement(sql);
+//			
+//			// 바꿀 주소
+//			pstmt.setString(1, id); 
+//			
+//			// 기존 주소
+//			pstmt.setString(2, MyPage.userId); 
+//			
+			result = pstmt.executeUpdate();
+//			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			DBUtil.close(con, pstmt);
+		}
+		return result;
+	}
+	
+	// 번호 변경
+	public int updatepghone(){
+		Connection con = DBUtil.getConnection();
+		PreparedStatement pstmt = null;
+		int result = 0;
+		String sql = "update table phone set colume = ? "
+				+ " where colume = ?";
+		try {
+//			
+//			pstmt = con.prepareStatement(sql);
+//			
+//			// 바꿀 번호
+//			pstmt.setString(1, id); 
+//			
+//			// 기존 번호
+//			pstmt.setString(2, MyPage.userId); 
+//			
+			result = pstmt.executeUpdate();
+//			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			DBUtil.close(con, pstmt);
+		}
+		return result;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+} // end of class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/com/team/mypage/MemberInfoService.java
+++ src/com/team/mypage/MemberInfoService.java
@@ -3,7 +3,7 @@ package com.team.mypage;
 import java.util.List;
 import java.util.Scanner;
 
-import com.team.dao.MemberDAO;
+import com.team.dao.MemberDao;
 import com.team.dto.MemberDTO;
 import com.team.util.Service;
 
@@ -13,7 +13,7 @@ public class MemberInfoService implements Service{
 	public void exec(Scanner scan) {
 		// TODO Auto-generated method stub
 		// 기존 회원정보 출력
-		MemberDAO member = new MemberDAO();
+		MemberDao member = new MemberDao();
 		
 		List<MemberDTO> list = member.memberList();
 		
@@ -27,17 +27,23 @@ public class MemberInfoService implements Service{
 		int num = scan.nextInt();
 		switch (num) {
 		case 1:
-			System.out.println("이름 수");
+			System.out.println("이름 수정");
+			String id = scan.next();
+//			member.updateName(id, Member.userId);
 			
 			break;
 		case 2:
+			System.out.println("이메일 수정");
+			String mail = scan.next();
 			
 			break;
 		case 3:
-			
+			System.out.println("주소 수정");
+			String addr = scan.next();
 			break;
 		case 4:
-			
+			System.out.println("전화번호 수정");
+			String phone = scan.next();
 			break;
 
 		default:
