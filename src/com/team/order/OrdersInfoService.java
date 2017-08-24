084901725d01f51a9f30a48e2629ead4a1265977
kosta <kosta@kosta-HP>
2017-08-24 10:46:50 +0900

    OrdersInfoServiece update

--- src/com/team/order/OrdersInfoService.java
+++ src/com/team/order/OrdersInfoService.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
 
+import com.team.cart.CartInfoService;
 import com.team.dao.OrdersDao;
 import com.team.dto.OrdersDTO;
 import com.team.util.Service;
@@ -15,30 +16,58 @@ public class OrdersInfoService implements Service {
 	public void exec(Scanner scan,String id) {
 		// TODO Auto-generated method stub
 		OrdersDTO odto = new OrdersDTO();
-
-		// 주문 번호
-
+		scan.nextLine();
 		// ID 받아 오기
+		System.out.println(" -----------------------------------------");
+		System.out.println("ID : " + id);
+		System.out.println(" -----------------------------------------");
 
-		// 배송지 주소 입력
-		System.out.println("배송지 주소 입력");
-		odto.setAddr(scan.nextLine());
+		
+		
+		boolean check = true;
+		while (check) {
 
-		// 수취인 이름
-		System.out.println("수취인 이름 입력");
-		odto.setName(scan.nextLine());
+			System.out.println(" --------------- 주문 하기 ---------------");
+			System.out.println("1. 주문정보 입력하기");
+			System.out.println("2. 뒤로가기");
+			System.out.println(" ----------------------------------------- ");
+			System.out.print(" 입력 : ");
+			int select = scan.nextInt();
+			scan.nextLine();
+			switch (select) {
+			case 1:
+				// 배송지 주소 입력
+				System.out.print("배송지 주소 입력 : ");
+				odto.setAddr(scan.nextLine());
+				
+				// 수취인 이름
+				System.out.println("수취인 이름 입력 : ");
+				odto.setName(scan.nextLine());
 
-		// 수취인 연락처
-		System.out.println("수취인 연락처 입력");
-		odto.setPhone(scan.nextLine());
+				// 수취인 연락처
+				System.out.println("수취인 연락처 입력 : ");
+				odto.setPhone(scan.nextLine());
 
-		// 주문 시간
-//		Date d = new Date();
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd kk:mm:ss");
-		String strDate = dateFormat.format(Calendar.getInstance().getTime());
+				// 주문 시간
+//				Date d = new Date();
+				SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd kk:mm:ss");
+				String strDate = dateFormat.format(Calendar.getInstance().getTime());
 
-		odto.setOrderTime(strDate);
-		odto.setId(id);
+				odto.setOrderTime(strDate);
+				odto.setId(id);
+				break;
+			
+			case 2:
+				new CartInfoService().exec(scan, id);
+				break;
+
+			default:
+				System.out.println("잘못된 번호를 입력하셨습니다. 다시 입력해 주세요.");
+				break;
+			}
+
+		}
+
 		
 		// DAO 클래스의 정보 입력 메소드 호출
 		OrdersDao.add(odto);
